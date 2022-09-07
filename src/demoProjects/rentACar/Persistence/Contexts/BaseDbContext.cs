aad1a0f94888126524ad0446f1da48c848b3ebea
MURAT AĞYÜZ <182804010@ogr.cbu.edu.tr>
2022-09-07 22:06:03 +0300

    Add Model Entity

--- src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
+++ src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
@@ -8,7 +8,8 @@ namespace Persistence.Contexts
     {
         protected IConfiguration Configuration { get; set; }
         public DbSet<Brand> Brands { get; set; }
-        
+        public DbSet<Model> Models { get; set; }
+
         public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
         {
             Configuration = configuration;
@@ -23,15 +24,31 @@ namespace Persistence.Contexts
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Brand>(a =>
+            modelBuilder.Entity<Brand>(b =>
             {
-                a.ToTable("Brands").HasKey(k => k.Id);
-                a.Property(p => p.Id).HasColumnName("Id");
-                a.Property(p => p.Name).HasColumnName("Name");
+                b.ToTable("Brands").HasKey(b => b.Id);
+                b.Property(b => b.Id).HasColumnName("Id");
+                b.Property(b => b.Name).HasColumnName("Name");
+                b.HasMany(b => b.Models);
+            });
+
+            modelBuilder.Entity<Model>(m =>
+            {
+                m.ToTable("Models").HasKey(m => m.Id);
+                m.Property(m => m.Id).HasColumnName("Id");
+                m.Property(m => m.BrandId).HasColumnName("BrandId");
+                m.Property(m => m.Name).HasColumnName("Name");
+                m.Property(m => m.DailyPrice).HasColumnName("DailyPrice");
+                m.Property(m => m.ImageUrl).HasColumnName("ImageUrl");
+                m.HasOne(m => m.Brand);
             });
 
             Brand[] brandEntitySeeds = { new(1, "BMW"), new(2, "Mercedes") };
             modelBuilder.Entity<Brand>().HasData(brandEntitySeeds);
+
+
+            Model[] modelEntitySeeds = { new(1, 1, "Series 4", 1500, ""), new(2, 1, "Series 3", 1200, ""), new(3, 2, "A180", 1000, "") };
+            modelBuilder.Entity<Model>().HasData(modelEntitySeeds);
         }
     }
 }
